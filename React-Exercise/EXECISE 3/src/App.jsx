893bcc44928325a67e4fafd7337e84180e4525e4
kuusoow <kussoow@gmail.com>
2026-08-24 10:09:15 +0300

    Fix exercise 3 structure

---
+++ React-Exercise/EXECISE 3/src/App.jsx
@@ -0,0 +1,21 @@
+import UserCard from'./UserCard.jsx'
+export default function App() { 
+  return (
+    <>
+    <UserCard
+       userName='Abdullahi Ali'
+       email='abdullahi.ali@example.com'
+    />
+    <UserCard
+       userName='John Doe'
+       email='john.doe@example.com'
+    />
+    <UserCard
+    userName='Mc'
+    email='mc@example.com'
+    />
+    
+    </>
+  )
+
+}
---
+++ React-Exercise/EXECISE 3/src/UserCard.jsx
@@ -0,0 +1,8 @@
+export default function UserCard({userName, email}) {
+  return (
+    <>
+      <h2>{userName}</h2>
+      <p>{email}</p>
+    </>
+  )
+}
