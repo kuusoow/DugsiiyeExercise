1d4aee7635052ffc05855da6a44ed6be556b9010
kuusoow <kussoow@gmail.com>
2026-08-28 15:13:43 +0300

    Add exercise 5

--- React-Exercise/exercise 9/src/App.jsx
+++ React-Exercise/exercise 9/src/App.jsx
@@ -3,17 +3,23 @@ export default function App(){
   const [usersData ,setUsersData] =useState(null)
   const [inputValue,setInputValue]=useState('')
   const [searchTerm ,setSearchTerm]=useState('')
+  const [loading, setLoading] =useState(false)
+  const[error,setEroor]=useState('')
    
    useEffect(function(){
       if( !searchTerm) return
         const fetchinguserData =async()=>{
           try{
             const response =await fetch(`https://api.github.com/users/${searchTerm}`)
+
+            if(!response.ok){throw new Error('GitHub user not found')}
             const data = await response.json()
             setUsersData(data)
 
           }catch(error){
-            console.error('error',error)
+            setEroor(error.message)
+          }finally{
+            setLoading(false)
           }
         }
         fetchinguserData()
@@ -24,6 +30,7 @@ export default function App(){
 
   function handleClick(){
    setSearchTerm(inputValue)
+   setLoading(true)
   }
   return(
 <>
@@ -31,15 +38,28 @@ export default function App(){
 <input type="text"
  placeholder="Enter Github Username"
  onFocus={()=>setInputValue('')}
+ value={inputValue}
  onChange={(e)=>setInputValue(e.target.value)}
  />
 <button onClick={handleClick}>Search</button>
 {
-  usersData &&
+loading ? <h1>Loading....</h1> 
+:error ?
+( <h1>Error:{error}</h1>  
+
+):
+
+  usersData ?(
   <>
   <h2>{usersData.name}</h2>
   <img src={usersData.avatar_url}/>
-  </>
+  <h4>{usersData.followers} <strong>Followers</strong></h4>
+  <h4>{usersData.following}<strong>Followeing</strong></h4>
+  <h4>{usersData.public_repos}<strong>repositiries</strong></h4>
+  <h4>{usersData.location}</h4>
+
+  </>) :null
+
 }
 </>
   )
